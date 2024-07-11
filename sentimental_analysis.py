906fba53dd8a314f7e5506409b692fae0d11a35e
aalia271530 <139776021+aalia271530@users.noreply.github.com>
2024-07-11 21:52:53 +0530

    Update sentimental_analysis.py

--- sentimental_analysis.py
+++ sentimental_analysis.py
@@ -8,24 +8,9 @@ def analyze_sentiment(text):
         return 'negative'
     else:
         return 'neutral'
-'''an article analysis'''
-'''from newspaper import Article
-
-url="https://www.ndtv.com/world-news/uk-woman-murdered-parents-lived-with-their-dead-bodies-for-several-years-6034977"
-article=Article(url)
-article.download()
-article.parse()
-article.nlp()
-
-text=article.summary
-text=article.summary
-print(text)'''
-
 
 '''read a file '''
 with open("sentimental_analysis/mytext_s_a.txt", 'r') as f:
     text=f.read()
-
-# blob=TextBlob(text)
-# sentiment=blob.sentiment.polarity
+    
 print(analyze_sentiment(text))
